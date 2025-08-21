165e43a7e1527fd1a023bf242d9bb7b3d7e2ff38
Aether <64797587+Aeastr@users.noreply.github.com>
2025-08-21 23:25:59 +0100

    Initial Commit

---
+++ llm icon playground/llm_icon_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  llm_icon_playgroundApp.swift
+//  llm icon playground
+//
+//  Created by Aether on 21/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct llm_icon_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
